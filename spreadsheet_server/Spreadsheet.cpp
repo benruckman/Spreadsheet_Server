103017ed5264344f6e50bc1344769453a21ce6df
jacksonjaymckay <70169105+jacksonjaymckay@users.noreply.github.com>
2021-04-26 15:19:27 -0600

    Server sends disconnects and the entire spreadsheet upon a connection.

--- spreadsheet_server/Spreadsheet.cpp
+++ spreadsheet_server/Spreadsheet.cpp
@@ -12,7 +12,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <bits/stdc++.h>
-//#include <boost/algorithm/string.hpp>
+ //#include <boost/algorithm/string.hpp>
 #include "spreadsheet.h"
 #include <set>
 
@@ -30,24 +30,25 @@ using namespace std;
  */
 spreadsheet::spreadsheet(string name)
 {
-  // Initialize variables
-  this->spreadsheet_name = name;
-  
-  map<string, string> cells;
-  this->non_empty_cells = cells;
-  
-  queue<cell> history;
-  this->cell_history = history;
-  
-  queue<string> messages;
-  this->message_queue = messages;
-  
-  vector<user> users;
-  this->user_list = users;
+	// Initialize variables
+	this->spreadsheet_name = name;
 
-  // Open the spreadsheet if it exists
-  open_spreadsheet(name);
+	map<string, string> cells;
+	this->non_empty_cells = cells;
+
+	queue<cell> history;
+	this->cell_history = history;
+
+	queue<string> messages;
+	this->message_queue = messages;
+
+	vector<user> users;
+	this->user_list = users;
+
+	// Open the spreadsheet if it exists
+	open_spreadsheet(name);
 }
+
 
 spreadsheet::spreadsheet()
 {
@@ -66,23 +67,23 @@ spreadsheet::~spreadsheet()
 
 /*
  * Returns the spreadsheet name
- 
+
  * @return the spreadsheet name
  */
 string spreadsheet::get_name()
 {
-  return this->spreadsheet_name;
+	return this->spreadsheet_name;
 }
 
 
 /*
  * Sets the spreadsheet name to the provided string
- 
+
  * @param name - The new spreadsheet name
  */
 void spreadsheet::set_name(string name)
 {
-  this->spreadsheet_name = name;
+	this->spreadsheet_name = name;
 }
 
 
@@ -93,10 +94,10 @@ void spreadsheet::set_name(string name)
  */
 void spreadsheet::add_message(string new_message)
 {
-  pthread_mutex_lock(&mutexqueue);
-  this->message_queue.push(new_message);
-  pthread_mutex_unlock(&mutexqueue);
-  return;
+	pthread_mutex_lock(&mutexqueue);
+	this->message_queue.push(new_message);
+	pthread_mutex_unlock(&mutexqueue);
+	return;
 }
 
 
@@ -108,14 +109,14 @@ void spreadsheet::add_message(string new_message)
  */
 string spreadsheet::get_cell_contents(string name)
 {
-  //googled stackoverflow for how to iterate through a map
-  for(map<string, string>::iterator it = non_empty_cells.begin(); it != non_empty_cells.end(); it++)
-  {
-    //it->first is the key of the map
-    if(it->first == name)
-      return non_empty_cells[name];
-  }
-  return "";
+	//googled stackoverflow for how to iterate through a map
+	for (map<string, string>::iterator it = non_empty_cells.begin(); it != non_empty_cells.end(); it++)
+	{
+		//it->first is the key of the map
+		if (it->first == name)
+			return non_empty_cells[name];
+	}
+	return "";
 }
 
 
@@ -124,15 +125,15 @@ string spreadsheet::get_cell_contents(string name)
  */
 vector<string> spreadsheet::get_names_of_all_non_empty_cells()
 {
-  vector<string> cells;
-  
-  //this is the way to iterate through a map
-  for(map<string, string>::iterator it = non_empty_cells.begin(); it != non_empty_cells.end(); it++)
-  {
-    cells.push_back(it->first);
-  }
-  
-  return cells;
+	vector<string> cells;
+
+	//this is the way to iterate through a map
+	for (map<string, string>::iterator it = non_empty_cells.begin(); it != non_empty_cells.end(); it++)
+	{
+		cells.push_back(it->first);
+	}
+
+	return cells;
 }
 
 
@@ -141,30 +142,35 @@ vector<string> spreadsheet::get_names_of_all_non_empty_cells()
  */
 bool spreadsheet::set_contents_of_cell(string name, string content, bool undo)
 {
-  //used to keep the previous contents of a cell
-  string previous_contents = "";
-  
-  if(undo == false && non_empty_cells.size() != 0)
-  {
-    for(map<string, string>::iterator it = non_empty_cells.begin(); it != non_empty_cells.end(); it++)
-    {
-      //if the key in the map is equal to name of the cell
-      if(it->first == name)
-      {
-	      //it->second is the value associated with the key
-	      previous_contents = it->second;
-	      set<string> newDependents;
-	      
-      }
-    }
-    
-    non_empty_cells[name] = content;
-    //needs to check if the content is a formula, if it is, then we would need a way to return variable in the formula
-    //then check for circular dependencies and replace dependents and add dependencies 
-    // if(content[0] == "=")
-    
-    return true; // TODO: Determine correct return value
-  }
+	//used to keep the previous contents of a cell
+	string previous_contents = "";
+
+	if (undo == false && non_empty_cells.size() != 0)
+	{
+		for (map<string, string>::iterator it = non_empty_cells.begin(); it != non_empty_cells.end(); it++)
+		{
+			//if the key in the map is equal to name of the cell
+			if (it->first == name)
+			{
+				//it->second is the value associated with the key
+				previous_contents = it->second;
+				set<string> newDependents;
+
+			}
+		}
+
+		non_empty_cells[name] = content;
+		//needs to check if the content is a formula, if it is, then we would need a way to return variable in the formula
+		//then check for circular dependencies and replace dependents and add dependencies 
+		// if(content[0] == "=")
+
+		return true; // TODO: Determine correct return value
+	}
+	 //if this is the first edit -- THIS was for testing
+	else if (non_empty_cells.size() == 0) 
+	{
+		non_empty_cells[name] = content;
+	}
 }
 
 
@@ -173,7 +179,7 @@ bool spreadsheet::set_contents_of_cell(string name, string content, bool undo)
  */
 void spreadsheet::add_user(user* new_user)
 {
-  user_list.push_back(*new_user);
+	user_list.push_back(*new_user);
 }
 
 
@@ -182,14 +188,14 @@ void spreadsheet::add_user(user* new_user)
  */
 void spreadsheet::remove_user(int id)
 {
-  for(vector<user>::iterator it = user_list.begin(); it != user_list.end(); it++)
-  {
-    if(it->get_id() == id)
-    {
-      user_list.erase(it);
-      break; // this fixes something, not sure why? -Jackson
-    }
-  }
+	for (vector<user>::iterator it = user_list.begin(); it != user_list.end(); it++)
+	{
+		if (it->get_id() == id)
+		{
+			user_list.erase(it);
+			break; // this fixes something, not sure why? -Jackson
+		}
+	}
 }
 
 
@@ -198,18 +204,18 @@ void spreadsheet::remove_user(int id)
  */
 string spreadsheet::undo_spreadsheet()
 {
-  cell previous_cell = cell_history.front();
-  
-  cell_history.pop();
-  
-  string cell_name = previous_cell.cell_name;
-  string cell_content = previous_cell.cell_contents;
-  
-  set_contents_of_cell(cell_name, cell_content, true);
-  
-  string new_change = cell_name + " : " + cell_content;
-  
-  return new_change;
+	cell previous_cell = cell_history.front();
+
+	cell_history.pop();
+
+	string cell_name = previous_cell.cell_name;
+	string cell_content = previous_cell.cell_contents;
+
+	set_contents_of_cell(cell_name, cell_content, true);
+
+	string new_change = cell_name + " : " + cell_content;
+
+	return new_change;
 }
 
 
@@ -218,23 +224,22 @@ string spreadsheet::undo_spreadsheet()
  */
 void spreadsheet::save()
 {
-  ofstream f;
-  
-  string file_name = "./../spreadsheet_data/" + spreadsheet_name + ".txt";
-  
-  //needs to pass in a c string to open the file
-  f.open(file_name.c_str());
-  
-  //iterate through the nonempty cell and write them to a text file
-  for(map<string, string>::iterator it = non_empty_cells.begin(); it != non_empty_cells.end(); it++)
-  { 
-    cout << "Hello from save" << endl;
-    //it->first is the cell name in the map, it->second is the contents of the cell in the map
-    string cells = it->first + " " + it->second + "\n";
-    f<<cells;
-  }
-  
-  f.close();
+	ofstream f;
+
+	string file_name = "./../spreadsheet_data/" + spreadsheet_name + ".txt";
+
+	//needs to pass in a c string to open the file
+	f.open(file_name.c_str());
+
+	//iterate through the nonempty cell and write them to a text file
+	for (map<string, string>::iterator it = non_empty_cells.begin(); it != non_empty_cells.end(); it++)
+	{
+		//it->first is the cell name in the map, it->second is the contents of the cell in the map
+		string cells = it->first + " " + it->second + "\n";
+		f << cells;
+	}
+
+	f.close();
 }
 
 
@@ -243,39 +248,39 @@ void spreadsheet::save()
  */
 map<string, string> spreadsheet::open_spreadsheet(string file_name)
 {
-  string sheet_name = file_name + ".txt";
-  ifstream f;
-  string cell_name = "";
-  string cell_contents;
-  
-  // Open the file
-  f.open(sheet_name.c_str());
-  
-  while(f>>cell_name>>cell_contents)
-  {
-    cout << cell_name << " " << cell_contents << endl;
-    set_contents_of_cell(cell_name, cell_contents, true);
-  }
-  
-  f.close();
-  
-  return non_empty_cells;
+	string sheet_name = "./../spreadsheet_data/" + file_name + ".txt";
+	ifstream f;
+	string cell_name = "";
+	string cell_contents = "";
+
+	// parse our data from our file
+	std::ifstream file(sheet_name);
+	for (std::string line; std::getline(file, line);)
+	{
+		size_t last_index = line.find_first_of(" ");
+		cell_name = line.substr(0, last_index);
+		cell_contents = line.substr(cell_name.length() + 1);
+		cout << cell_name << " " << cell_contents << endl;
+		set_contents_of_cell(cell_name, cell_contents, true);
+		cout << non_empty_cells.size() << std::endl;
+	}
+	return non_empty_cells;
 }
 
 
-/*
+/*./a.
  * TODO: Document
  */
 string spreadsheet::normalize(string cell_contents)
 {
-  string normalized_contents;
-  
-  for(int i = 0; i < cell_contents.size(); i++)
-  {
-    normalized_contents += toupper(cell_contents[i]);
-  }
-  
-  return normalized_contents;
+	string normalized_contents;
+
+	for (int i = 0; i < cell_contents.size(); i++)
+	{
+		normalized_contents += toupper(cell_contents[i]);
+	}
+
+	return normalized_contents;
 }
 
 
@@ -284,25 +289,24 @@ string spreadsheet::normalize(string cell_contents)
  */
 bool spreadsheet::process_messages()
 {
-  pthread_mutex_lock(&mutexqueue);
-  while(message_queue.size()> 0)
-  {
-    string message = message_queue.front();
-    this->message_queue.pop();
-    std::cout << message << std::endl;
-    int n = message.length();
-    // manipulate message if it is a valid request
-    char m[n + 1];
-    strcpy(m, message.c_str());
-    for(vector<user>::iterator it = user_list.begin(); it != user_list.end(); it++)
-    {
-        // send the message to connected users
-      char* mess = "{\"messageType\" : \"cellUpdated\", \"cellName\" : \"a1\", \"contents\" : \"=1 + 3\"}\n";
-      send(it->get_socket(), mess, strlen(mess), 0);
-    }
-  }
-  pthread_mutex_unlock(&mutexqueue);
-  return true;
+	pthread_mutex_lock(&mutexqueue);
+	while (message_queue.size() > 0)
+	{
+		string message = message_queue.front();
+		this->message_queue.pop();
+		std::cout << message << std::endl;
+		int n = message.length();
+		// manipulate message if it is a valid request
+		char m[n + 1];
+		strcpy(m, message.c_str());
+		for (vector<user>::iterator it = user_list.begin(); it != user_list.end(); it++)
+		{
+			// TODO: parse and send correct message
+			//send(it->get_socket(), mess, strlen(mess), 0);
+		}
+	}
+	pthread_mutex_unlock(&mutexqueue);
+	return true;
 }
 
 /*
@@ -310,21 +314,21 @@ bool spreadsheet::process_messages()
  */
 vector<string> spreadsheet::get_variables(string contents)
 {
-  vector<string> input;
-  vector<string> variables;
-  //REFERENCE: geeksforgeeks.org/boostsplit-c-library/
-  //boost::split(input, contents, boost::is_any_of("-|+|/|*|="));
-  for(vector<string>::iterator it = input.begin(); it != input.end(); it++)
-  {
-    //REFERENCE: tutorialspoint.com/c_standard_library/c_function_atoi.htm
-    //atoi returns 0 if the string is not converted to an integer, thus it is a variable
-    int val = atoi((*it).c_str());
-    if(val == 0)
-    {
-      variables.push_back(*it);
-    }
-  }
-  return variables;
+	vector<string> input;
+	vector<string> variables;
+	//REFERENCE: geeksforgeeks.org/boostsplit-c-library/
+	//boost::split(input, contents, boost::is_any_of("-|+|/|*|="));
+	for (vector<string>::iterator it = input.begin(); it != input.end(); it++)
+	{
+		//REFERENCE: tutorialspoint.com/c_standard_library/c_function_atoi.htm
+		//atoi returns 0 if the string is not converted to an integer, thus it is a variable
+		int val = atoi((*it).c_str());
+		if (val == 0)
+		{
+			variables.push_back(*it);
+		}
+	}
+	return variables;
 }
 
 /*
@@ -332,5 +336,43 @@ vector<string> spreadsheet::get_variables(string contents)
  */
 DependencyGraph spreadsheet::get_dependency_graph()
 {
-  return g;
+	return g;
+}
+
+// sends the entire spreadsheet to a socket
+void spreadsheet::send_spreadsheet(int socket)
+{
+	for (map<string, string>::iterator it = non_empty_cells.begin(); it != non_empty_cells.end(); it++)
+	{
+		std::string s;
+		s.append("{\"messageType\" : \"cellUpdated\", \"cellName\" : \"");
+		s.append(it->first);
+		s.append("\", \"contents\" : \"");
+		s.append(it->second);
+		s.append("\"}\n");
+
+		int n = s.length();
+		char message[n + 1];
+		strcpy(message, s.c_str());
+		send(socket, message, strlen(message), 0);
+	}
+}
+
+void spreadsheet::send_disconnect(int ID)
+{
+	std::cout << "SENDING DISCONNECT" << std::endl;
+	std::string id = std::to_string(ID);
+	std::string s;
+	s.append("{\"messageType\" : \"disconnected\", \"user\" : \"");
+	s.append(id);
+	s.append("\"}\n");
+	int n = s.length();
+	char message[n + 1];
+	strcpy(message, s.c_str());
+	for (vector<user>::iterator it = user_list.begin(); it != user_list.end(); it++)
+	{
+		std::cout << "SENDING DISCONNECT TO USER "<< it->get_username() << std::endl;
+		send(it->get_socket(), message, strlen(message), 0);
+	}
+
 }
